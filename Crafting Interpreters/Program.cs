23fc890b5920e32af0bf39b136bfad0b87687959
maxumi <maxjac64@gmail.com>
2024-04-09 18:14:22 +0200

    Made it to 4.1 with scanning

---
+++ Crafting Interpreters/Program.cs
@@ -0,0 +1,47 @@
+﻿using Crafting_Interpreters;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CraftingInterpreters.Lox
+{
+    class Lox
+    {
+        static public void Main(String[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: jlox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                runFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+
+        }
+        private static void runFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(Path.GetFullPath(path));
+            Run(Encoding.Default.GetString(bytes));
+        }
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string line = Console.ReadLine();
+                if (line == null) break;
+            }
+        }
+        private static void Run(string source)
+        {
+            Scanner scanner = new Scanner(source);
+            List<Token> tokens = scanner.ScanTokens();
+        }
+    }
+}
---
+++ Crafting Interpreters/Scanner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crafting_Interpreters
+{
+    internal class Scanner
+    {
+        private string source { get; set; } = "";
+        private List<Token> tokens = new List<Token>();
+
+
+        public Scanner(string source)
+        {
+            this.source = source;
+        }
+        // WIP
+        public List<Token> ScanTokens()
+        {
+            return new List<Token>();
+        }
+
+    }
+}
