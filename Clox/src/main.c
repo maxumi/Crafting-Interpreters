ad3f69bd2da39a179fdc5b4f7ab87a35ae5bf6be
maxumi <maxjac64@gmail.com>
2024-06-03 14:16:29 +0200

    Made it to 15.2.2

--- Clox/src/main.c
+++ Clox/src/main.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include "..\include\chunk.h"
 #include "..\include\debug.h"
+#include "../include/vm.h"
 
 
 int main(int argc, const char* argv[]) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
 
@@ -14,6 +16,8 @@ int main(int argc, const char* argv[]) {
 
   writeChunk(&chunk, OP_RETURN,123);
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
